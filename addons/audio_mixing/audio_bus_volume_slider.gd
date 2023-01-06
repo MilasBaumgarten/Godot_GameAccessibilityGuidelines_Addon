250a9b5430ca9b2b534d467f7eb30ca4c9e96669
Milas Baumgarten <milas.baumgarten@freenet.de>
2023-01-06 15:21:33 +0100

    change structure, minor improvements

--- addons/audio_mixing/audio_bus_volume_slider.gd
+++ addons/audio_mixing/audio_bus_volume_slider.gd
@@ -3,7 +3,7 @@ extends Slider
 
 export var bus_name: String = "Master"
 
-export var audio_duration_after_drag_ended: float = 0.5
+export var audio_duration_after_drag_ended: float = 0.75
 export(NodePath) var example_audio_path
 onready var example_audio = get_node(example_audio_path)
 
@@ -23,18 +23,16 @@ func _ready():
     
     # connect signals
     connect("value_changed", self, "_on_value_changed")
-    connect("drag_ended", self, "_on_drag_ended")
-    
-    # create timer
-    get_tree().create_timer(5.0)
 
 
 func _on_value_changed(value: float):
-    # disconnect currently running timer to not
-    # stop the audio while moving the slider
+    # "stop" the currently running timer, to continue playing audio while
+    # changing the volume
     if timer:
         if timer.is_connected("timeout", self, "_on_timeout"):
             timer.disconnect("timeout", self, "_on_timeout")
+    timer = get_tree().create_timer(audio_duration_after_drag_ended)
+    timer.connect("timeout", self, "_on_timeout")
 
     # set the volume in db or mute if min value was reached
     if value == self.min_value:
@@ -46,10 +44,6 @@ func _on_value_changed(value: float):
     if not example_audio.playing:
         example_audio.play()
 
-# create a timer after which the audio will stop playing
-func _on_drag_ended(value_changed: bool):
-    timer = get_tree().create_timer(audio_duration_after_drag_ended)
-    timer.connect("timeout", self, "_on_timeout")
 
 func _on_timeout():
     example_audio.stop()
